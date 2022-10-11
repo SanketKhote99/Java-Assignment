3a65726a598259989d0f46a36b8a9f29d8a8cb87
Sanket Khote <sanketkhote43@gmail.com>
2022-10-11 23:01:50 +0530

    Set a add

--- Assignment-1/Set-A/ArrayRev.java
+++ Assignment-1/Set-A/ArrayRev.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class ArrayRev {
     public static void main(String[] args) {
-        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements you want");
+        int count = sc.nextInt();
+        int [] array = new int[count];
+        System.out.println("Enter the elements of the array: ");  
+            for(int i=0; i<count; i++) {  
+                array[i]=sc.nextInt();  
+            }   
+
+        // array displays
         System.out.println("Original array: ");  
-        for (int i = 0; i < arr.length; i++) {  
-            System.out.print(arr[i] + " ");  
+        for (int i = 0; i < array.length; i++) {  
+            System.out.print(array[i] + " ");  
         }  
         
         // reverse order  
         System.out.println("\nArray in reverse order: ");  
-        for (int i = arr.length-1; i >= 0; i--) {  
-            System.out.print(arr[i] + " ");  
+        for (int i = array.length-1; i >= 0; i--) {  
+            System.out.print(array[i] + " ");  
         }  
     }  
 } 
